c85819bde48cef4ba02c77b44585f64c283fe518
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-06-03 17:20:07 +0500

    feat: Add main application structure with Index and About components, and implement styles for authentication and creation screens

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, world!</Text>
+      <View style={styles.separator}></View>
+      <Link style={styles.button} href="/about">
+        About
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+    backgroundColor: '#ccc'
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    paddingHorizontal: 40,
+    paddingVertical: 8,
+    borderRadius: 5,
+    textAlign: 'center'
+  }
+});
